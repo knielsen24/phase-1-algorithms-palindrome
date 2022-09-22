fed5914a27a89a80d784787f0490ea8de9e4fa56
Kevin Nielsen <knielsen24@gmail.com>
2022-09-22 15:51:56 -0700

    commit tree be292eee3876edf2c2e7d9fd6fe1bed19b4ddd91

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  const lowerCaseWord = word.toLowerCase()
+  const charIteration = Math.ceil(word.length / 2)
+  debugger  
+  for (let i = 0; i < charIteration; i++) {
+    const j = lowerCaseWord.length - 1 - i    
+    console.log(lowerCaseWord[i])
+    console.log(lowerCaseWord[j])
+    return lowerCaseWord[i] === lowerCaseWord[j]
+    }
+  }
+
+isPalindrome("racecar")
+
+// // You can run `node index.js` to view these console logs
+// if (require.main === module) {
+//   // add your own custom tests in here
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("racecar"));
+
+//   console.log("");
+
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("robot"));
+//   console.log("");
+
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("racezcar"));
+// }
+
+// module.exports = isPalindrome;
+
+
 
 /* 
-  Add your pseudocode here
+  Palindrome is going to take a string of words and look at each letter.
+  it will look at each index and determine if the inverse is the same character
+
 */
 
 /*
-  Add written explanation of your solution here
+  1) takes both strings and converts all letters to lower case
+      Inverse formulas
+        create a new array that takes in the characters of each letter
+  2) formula begins with the front end and the backend
+  3) it should iterate (string.length/2) and round up if odd number Math.Ceil
+  4) boolean (index 1 === string.length) it compares the two different characters with a boolean statement
+        string + X === string.length - x
+  5)set a counter++ on the front end and set a counter-- on the back end
+  6) the first false statement should stop the code
 */
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
